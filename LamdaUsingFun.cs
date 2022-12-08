863eb4826051c5c93fb015269479f6c9651c51f5
GlorySRaju <119593854+GlorySRaju@users.noreply.github.com>
2022-12-08 11:58:53 +0530

    Add files via upload

---
+++ LamdaUsingFun.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LamdaFuncCalculator
+{
+    class LamdaUsingFun
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int, int, int> addNum = (int firstNumber, int secondNumber)=>
+           
+                 firstNumber + secondNumber;
+           
+
+            Func<int, int, int> subtractNum = (int firstNumber, int secondNumber)=>
+
+                 firstNumber - secondNumber;
+            
+            Func<int, int, int> productNum = (int firstNumber, int secondNumber)=>
+          
+                 firstNumber * secondNumber;
+            
+
+            Func<int, int, int> divisionNum = (int firstNumber, int secondNumber)=>
+            
+                 firstNumber / secondNumber;
+            
+
+            Calculator(addNum, 10, 3);
+
+            Calculator(subtractNum, 10, 3);
+
+            Calculator(productNum, 10, 3);
+
+            Calculator(divisionNum, 10, 3);
+
+        }
+        static void Calculator(Func<int, int, int> cal, int firstNumber, int secondNumber)
+        {
+            Console.WriteLine(cal(firstNumber, secondNumber));
+        }
+    }
+}
