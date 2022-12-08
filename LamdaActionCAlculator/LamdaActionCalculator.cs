8716c6445a78b06244fc6e5835411a08b1f6c883
glory.sr <glory.sr@PIA3249>
2022-12-08 11:32:31 +0530

    Add project files.

---
+++ LamdaActionCAlculator/LamdaActionCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LamdaActionCAlculator
+{
+    class LamdaActionCalculator
+    {
+        static void Main(string[] args)
+        {
+            Action<int, int> addNum = (firstNumber, secondNumber)=>
+ 
+                Console.WriteLine(firstNumber + secondNumber);
+           
+
+            Action<int, int> subtractNum =  (firstNumber, secondNumber)=>
+            
+                Console.WriteLine(firstNumber - secondNumber);
+            
+
+            Action<int, int> productNum = (firstNumber, secondNumber)=>
+            
+                Console.WriteLine(firstNumber * secondNumber);
+            
+
+            Action<int, int> divisionNum = (firstNumber, secondNumber)=>
+           
+                Console.WriteLine(firstNumber / secondNumber);
+            
+
+            Calculator(addNum += subtractNum += productNum += divisionNum, 10, 3);
+
+
+        }
+        static void Calculator(Action<int, int> cal, int firstNumber, int secondNumber)
+        {
+            cal(firstNumber, secondNumber);
+        }
+    }
+}
